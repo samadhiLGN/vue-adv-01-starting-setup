0eda58aabf53dd9e30f86f9dc1c95773e50ba713
Yokai <samadhimadushanka@gmail.com>
2025-05-01 09:44:29 +0530

    addn component

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,12 @@
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue'
+
 export default {
+  components: {
+     TheHeader
+  },
   data() {
     return {
       activeUser: {
