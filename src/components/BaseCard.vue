d109add633f036d0bf62ff1813361355b8295d33
Yokai <samadhimadushanka@gmail.com>
2025-05-01 16:10:05 +0530

    add v-slots

--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -1,16 +1,28 @@
 <template>
     <div>
+        <header v-if="$slots.header">
+            <slot name="header">
+                <!-- <h2>The Default</h2> -->
+            </slot>
+        </header>
         <slot></slot>
     </div>
 </template>
 <script>
 
 export default {
-  
+  mounted(){
+    console.log(this.$slots.header);
+  }
 }
 </script>
 
 <style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 
 div {
   margin: 2rem auto;
--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -1,11 +1,14 @@
 <template>
   <section>
     <base-card>
-    <header>
+    <template v-slot:header>
       <h3>{{ fullName }}</h3>
       <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
-    </header>
-    <p>{{ infoText }}</p>
+    </template>
+    <template v-slot:default>
+      <p>{{ infoText }}</p>
+    </template>
+   
   </base-card>
   </section>
 </template>
@@ -19,9 +22,4 @@ export default {
 </script>
 
 <style scoped>
-section header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
 </style>
