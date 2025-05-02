c80fb9dc827f8954dbbf9721fea3b8d7b3588f4d
Yokai <samadhimadushanka@gmail.com>
2025-05-02 10:04:06 +0530

    add a scoped slots

---
+++ src/components/CourseGoals.vue
@@ -0,0 +1,17 @@
+<template>
+    <ul>
+        <li v-for="goal in goals" :key="goal">
+            <slot :item="goal" another-prop="..."></slot>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            goals: ['Finsh the course', 'Learn Vue', 'Before 14th May']
+        };
+    }
+}
+</script>
